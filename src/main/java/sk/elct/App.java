a656b310d46cafba228040b1c9332f3aed942840
miroslav-opiela <miroslav.opiela@upjs.sk>
2019-07-23 15:54:54 +0200

    utorok popoludni - riesene ulohy, stringy, referencne a primitivne premenne, pretecenie, pretypovanie,

--- src/main/java/sk/elct/App.java
+++ src/main/java/sk/elct/App.java
@@ -4,19 +4,40 @@ import sk.upjs.jpaz2.ObjectInspector;
 
 public class App {
     public static void main( String[] args ) {
-        String slovo = "utorok";
-        System.out.println(slovo);
-        System.out.println( "AHOJ Hello World!" );
-
-        //ObjectInspector oi = new ObjectInspector();
-        //oi.inspect(slovo);
-
-        String ineSlovo = slovo.replace('o', 'P');
-        System.out.println(ineSlovo);
-
         SmartRobot mark = new SmartRobot();
         System.out.println(mark.sing("tancuj vykrucaj", 'l'));
 
-        //oi.inspect(mark);
+        System.out.println(mark.abbreviation("ut"));
+        String res = mark.generateWord('A', 3);
+        System.out.println(res);
+        System.out.println(res.length());
+        System.out.println(mark.reverseString("abcd"));
+
+       /* System.out.println('z' > 'a');
+        System.out.println('z' > 'Z');
+        System.out.println('A' == 65);
+        System.out.println('Z' > 65);
+        System.out.println('A');
+        int cislo = 'A';
+        System.out.println(cislo);
+        char znak = 65;
+        System.out.println(znak);*/
+        System.out.println(mark.numbersCount("sdasda4545asddsa56"));
+        System.out.println(mark.hasSameCharactersMystery("aaaa"));
+
+        String a = "abc";
+        String b = "abc";
+        String c = new String("abc");
+        System.out.println(a == b);
+        System.out.println(a == c);
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+
+        mark.printBillions(10);
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(mark.randomDice());
+        }
+
     }
 }
--- src/main/java/sk/elct/SmartRobot.java
+++ src/main/java/sk/elct/SmartRobot.java
@@ -7,4 +7,82 @@ public class SmartRobot {
         song = song.replace('r', letter);
         return song;
     }
+
+    public String abbreviation(String s) {
+        // ak ma slovo malo pismen
+        if (s.length() < 3) {
+            return s.toUpperCase();
+        }
+
+        String abbreviation = s.substring(0, 3);
+        return abbreviation.toUpperCase();
+    }
+
+    public String generateWord(char c, int count) {
+        String s = "";
+        for (int i = 0; i < count; i++) {
+            s = s + c;
+            //s += c;
+        }
+        return s;
+    }
+
+    public String reverseString(String s) {
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            result = s.charAt(i) + result;
+        }
+        // alternativna verzia
+        /*for (int i = s.length() - 1; i >= 0; i--) {
+            result = result + s.charAt(i);
+        }*/
+        return result;
+    }
+
+    public int numbersCount(String s) {
+        // vyrobim si pocitadlo
+        int counter = 0;
+        // v cykle prejdem retazec
+        for (int i = 0; i < s.length(); i++) {
+            // ak je znak cislo, tak pripocitam 1
+            char c = s.charAt(i);
+            if (c >= '0' && c <= '9') {
+                counter++;
+            }
+        }
+        // vratim vysledok
+        return counter;
+    }
+
+    public boolean hasSameCharacters(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean hasSameCharactersMystery(String s) {
+        // vyrobit novy retazec dlzky s.length z pismen charAt(0)
+        String newString = this.generateWord(s.charAt(0), s.length());
+
+        // porovnat retazce
+        // toto porovnava referencie
+        // return s == newString;
+        return s.equals(newString);
+    }
+
+    public void printBillions(int count) {
+        // long namiesto int kvoli preteceniu
+        long number = 0;
+        for (int i = 0; i < count; i++) {
+            System.out.println(number);
+            number = number + 1000000000;
+        }
+    }
+
+    public int randomDice() {
+        return (int) (Math.random() * 6 + 1);
+    }
 }
